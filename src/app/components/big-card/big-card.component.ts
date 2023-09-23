4db8f072e4f891eef8b616a44a6195c209a3e893
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-09-23 10:53:39 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover: string = 'https://miro.medium.com/v2/resize:fit:1358/0*wuNf24urnMp7ypDp.png'
+  @Input()
+  cardTitle: string = 'SAIU A NOVA VERSÃO DO ANGULAR'
+  @Input()
+  cardDescription: string = 'Angular (comumente referido como "Angular 2+" ou "Angular 2") é uma plataforma de aplicações web de código-fonte aberto e front-end baseado em TypeScript liderado pela Equipe Angular do Google e por uma comunidade de indivíduos e corporações. Angular é uma reescrita completa do AngularJS, feito pela mesma equipe que o construiu.'
 
   constructor() { }
 
